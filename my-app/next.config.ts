40661a963ed7dd254aabda6007f6c4041d65760f
Tracyold <tracy.old.gold@gmail.com>
2026-05-22 20:56:57 +0000

    setup next build

--- my-app/next.config.ts
+++ my-app/next.config.ts
@@ -1,7 +1,33 @@
-import type { NextConfig } from "next";
+import type { NextConfig } from 'next';
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  turbopack: {
+    resolveExtensions: ['.matr', '.life', '.tsx', '.ts', '.jsx', '.js'],
+    rules: {
+      '*.matr': {
+        as: '*.tsx',
+      },
+      '*.life': {
+        as: '*.tsx',
+      },
+      '*.loc': {
+        loaders: ['sass-loader'],
+        as: '*.scss',
+      },
+      '*.dec': {
+        loaders: ['sass-loader'],
+        as: '*.scss',
+      },
+      '*.eff': {
+        loaders: ['sass-loader'],
+        as: '*.scss',
+      },
+      '*.root': {
+        loaders: ['sass-loader'],
+        as: '*.scss',
+      },
+    },
+  },
 };
 
 export default nextConfig;
---
+++ my-app/src/app/page.tsx
@@ -0,0 +1,6 @@
+// src/app/page.tsx
+import { Test } from './components/matr/test.matr';
+
+export default function Page() {
+  return <Test>hello</Test>;
+}
