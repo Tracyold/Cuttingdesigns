05b7768fe2b73f92315a56f65552599793cb5f55
Tracyold <tracy.old.gold@gmail.com>
2026-05-23 09:35:05 +0000

    added scripts, locs and decs next build

--- my-app/src/app/page.tsx
+++ my-app/src/app/page.tsx
@@ -1,5 +1,5 @@
 // src/app/page.tsx
-import { Test } from './components/matr/test.matr';
+import { Test } from './components/matr/test.tsx';
 
 export default function Page() {
   return <Test>hello</Test>;
